70acc462f98f48b080a91e4e6f2ff8bdb7ad7e57
unknown <linda.frischknecht@bluewin.ch>
2024-03-21 11:39:19 +0100

    group coding session

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,17 +1,21 @@
-const TaskForm = ({ newTodo, onNewTodoChange, onFormSubmit }) => {
+const TaskForm = ({ newTodo, handleNewTodoChange, onFormSubmit }) => {
   return (
     <form onSubmit={onFormSubmit}>
       <h1>📝 ToDo App</h1>
       <h2>Type you tasks here below 👇</h2>
-
-      <textarea
-        value={newTodo}
-        onChange={onNewTodoChange}
-        placeholder="Type a task.."
-      />
+      <label>
+        <textarea
+          rows="4"
+          cols="50"
+          type="text"
+          value={newTodo}
+          onChange={handleNewTodoChange}
+          placeholder="Type a task.."
+        />
+      </label>
       <button type="submit">Submit form!</button>
     </form>
-  )
-}
+  );
+};
 
-export default TaskForm
+export default TaskForm;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,32 +1,58 @@
-import { useState } from "react"
-import TaskForm from "./TaskForm"
-import TaskList from "./TaskList"
+import { useState, useEffect } from "react";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 export const Tasks = () => {
-  const [taskList, setTaskList] = useState([])
+  const [taskList, setTaskList] = useState([]);
   // define the setting state functions
-  const [loading] = useState(false)
-  const [newTodo] = useState("")
+  const [loading] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
 
   // fetch tasks
-  const fetchTasks = async () => {
-    // set task list to be saved in the state
-  }
-  
-  const handleNewTodoChange = () => {
-    // set a  new ToDo from the value of the textarea defined in the TaskForm component
-  }
 
-  const onFormSubmit = async () => {
+  const fetchTasks = async () => {
+    fetch("https://week-7-backend.onrender.com/tasks")
+      .then((result) => result.json())
+      .then((task) => {
+        setTaskList(task);
+      });
+    // set task list to be saved in the state
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const handleNewTodoChange = (event) => {
+    // set a  new ToDo from the value of the textarea defined in the TaskForm component
+    setNewTodo(event.target.value);
+  };
+
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
     // define your POST request for new ToDo
     // don't forget to set the loading state
-  }
+    fetch("https://week-7-backend.onrender.com/tasks", {
+      method: "POST",
+      body: JSON.stringify({
+        description: newTodo,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((result) => result.json)
+      .then((newData) => {
+        setNewTodo((previousData) => [newData, ...previousData]);
+      })
+      .catch((error) => {
+        console.error("Error fetching the data:", error);
+      });
+  };
 
   return (
     <div className="wrapper">
       <TaskForm
         newTodo={newTodo}
-        onNewTodoChange={handleNewTodoChange}
+        handleNewTodoChange={handleNewTodoChange}
         onFormSubmit={onFormSubmit}
       />
       <TaskList
@@ -35,5 +61,5 @@ export const Tasks = () => {
         setTaskList={setTaskList}
       />
     </div>
-  )
-}
+  );
+};
